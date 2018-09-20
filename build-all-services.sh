16609ffc23ef1d934bfa0967e69ebb8b91e4b2e5
MD Dubey <mddubey409@gmail.com>
2018-09-20 13:24:03 +0530

    Dubey | SHR-000 | Fix build-all-services

--- build-all-services.sh
+++ build-all-services.sh
@@ -11,27 +11,6 @@ cd ../MCI-Service/
 ./gradlew clean dist -x test
 cd ../HIE-Docker
 
-#todo MCI-Admin
-#echo "Build MCI Server"
-#cd ../MCI-Service/
-#./gradlew clean dist -x test
-#cd ../HIE-Docker
-
-echo "Build MCI-Backgroud-Jobs Server"
-cd ../MCI-Backgroud-Jobs/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
-echo "Build MCI-LR Server"
-cd ../MCI-LR/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
-echo "Build MCI-Registry Server"
-cd ../MCI-Registry/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
 echo "Build HealthId-Service Server"
 cd ../HealthId-Service/
 ./gradlew clean dist -x test
@@ -42,24 +21,8 @@ cd ../Identity-Server/
 ./gradlew clean dist -x test
 cd ../HIE-Docker
 
-echo "Build PatientJournal Server"
-cd ../PatientJournal/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
-echo "Build Datasense Server"
-cd ../SHR-Datasense/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
 echo "Build Terminology-Server Server"
 cd ../Terminology-Server/
 ./gradlew clean dist -x test
 cd ../HIE-Docker
-
-echo "Build freeshr-update Server"
-cd ../freeshr-update/
-./gradlew clean dist -x test
-cd ../HIE-Docker
-
 
